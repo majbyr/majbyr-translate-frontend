c72e621aaf902fb7536523417d31b75e600c567c
Aleksei Ivanov <aleksei@majbyr.com>
2024-03-31 05:57:13 +0300

    Update translations and meta tags

--- src/App.js
+++ src/App.js
@@ -21,6 +21,9 @@ function App() {
   const [isLoading, setIsLoading] = useState(true);
 
   useEffect(() => {
+    document
+      .querySelector('meta[name="description"]')
+      .setAttribute("content", t("description"));
     document.title = t("app");
   }, [t]);
 
